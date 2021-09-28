e52f7fdfdb0cae1dbb0432f807112c3158af6241
Benjamin <ben.d.maryland@gmail.com>
2021-09-28 08:54:41 -0400

    commit tree 9e251ea80e890353487c2cc8b7e234b9a2134ca2

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "pink";
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+  function moveDodgerRight() {
+    console.log("hi")
+    const rightNumbers = dodger.style.left.replace("px", "");
+    console.log(rightNumbers)
+    const right = parseInt(rightNumbers, 10);
+    console.log(right)
+    if (right < 360) {
+        dodger.style.left = `${right + 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        console.log(e.key)
+      moveDodgerRight();
+    }
+  });
+
+
+
+  
